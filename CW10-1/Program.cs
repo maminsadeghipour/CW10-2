5b713b7e2c113b16083404df3df40f3ea63cb999
luckyguy <aminsadeghiipour@gmail.com>
2023-12-01 11:46:12 +0330

    firstcommit

---
+++ CW10-1/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace CW10_1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+    }
+
+    private void PrintWelcome(string message)
+    {
+        Console.WriteLine(message);
+    }
+}
+
